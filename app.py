eb47e967415648c4266bba12ed1a4727217b0a04
MauroAntonioBogado1990 <mauroantoniobogado@gmail.com>
2025-06-22 21:09:46 -0300

    add new feature in agent using ollama with question user

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ import ollama
 
 client = ollama.Client()
 
-response = client.generate(model="llama3.2:1b", prompt="Crea una funcion en python para poder realizar un saludo")
+#se le agrega pregurnta de usuario
+pregunta_usuario = input("Pregunta sobre que camino de aprendizaje quieres hacer \n")
+
+response = client.generate(model="llama3.2:1b", prompt=pregunta_usuario)
 
 print("Codigo generado")
 print(response['response'])
